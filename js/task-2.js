f992198878ca9fc5dce5e87f636ac90b3cdfb5e5
uliacop <115625925+uliacop@users.noreply.github.com>
2024-02-28 13:52:48 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+console.log('Задача 2. Склад');
+class Storage {
+  #items;
+  constructor(items = []) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
